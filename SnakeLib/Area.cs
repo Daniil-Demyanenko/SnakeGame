f64e2c59464746cf7bace3b43b829e4e214abd88
xlopecemail <xlopecemail@gmail.com>
2022-01-10 21:18:48 +0300

    Добавил метод создания яблока

--- SnakeLib/Area.cs
+++ SnakeLib/Area.cs
@@ -12,17 +12,13 @@ public class Area
     {
         this.W = W; this.H = H;
         AreaCenter = new Point(W / 2, H / 2);
-
-        //TODO: Нормальный спавн яблока
-        Apple = new Point(rand.Next(W/2),rand.Next(H/2));
     } 
 
+
     public void add_apple()//TODO: Используй нормальное определение ячеек змеи 
-    {
-        Apple = new Point(rand.Next(W), rand.Next(H));
-        if (Apple.x == AreaCenter.x || Apple.y == AreaCenter.y) Apple.x++;
-
-
+    {        
+       do Apple = new Point(rand.Next(W), rand.Next(H));
+       while(!snake.IsContainPoint(Apple));
     }
     
 }
